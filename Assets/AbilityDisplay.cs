3ae1e71649ba00eeda98f835c8f4b3248f719699
sergio1811 <pekeno1811@gmail.com>
2019-04-24 16:38:57 +0200

    Scriptable Objects

---
+++ Assets/AbilityDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityDisplay : MonoBehaviour
+{
+    public Abilities Ability;
+
+    public Image Icon;
+    public Text Name;
+    public Text Description;
+
+
+    void Start()
+    {
+        Icon.sprite = Ability.Thumbnail;
+
+        Name.text = Ability.Name;
+
+        Description.text = Ability.Description;
+    }
+    
+}
---
+++ Assets/Scripts/SO/Abilities.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = ("Ability"), menuName = ("Ability"))]
+public class Abilities : ScriptableObject
+{
+    public string Name;
+    public string Description;
+    public Sprite Thumbnail;
+}

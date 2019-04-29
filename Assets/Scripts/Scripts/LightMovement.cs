a3666e1bfe695f67390897b608e34ead85d09a9e
Yaiza <yaizacastillo@users.noreply.github.com>
2019-04-29 19:56:59 +0200

    Movimiento luz
    Movimiento circular  y rotación sobre el vector 0, 0, 0

--- Assets/Scripts/Scripts/LightMovement.cs
+++ Assets/Scripts/Scripts/LightMovement.cs
@@ -7,19 +7,24 @@ public class LightMovement : MonoBehaviour
     public float timeToComplete;
     public float radius;
     float speed;
+    public float startAngle;
     float currentAngle;
+    public float degreesToRotate;
 
     void Start()
     {
-        speed = (Mathf.PI * 2) / timeToComplete;
+        currentAngle = startAngle;
+
+        transform.position = new Vector3(radius * Mathf.Cos(currentAngle), radius * Mathf.Sin(currentAngle), transform.position.z);
+
+        speed = (Mathf.Deg2Rad * degreesToRotate) / timeToComplete;
     }
 
     void Update()
     {
         currentAngle += Time.deltaTime * speed;
-        float x = radius * Mathf.Cos(currentAngle);
-        float y = radius * Mathf.Sin(currentAngle);
-        transform.position = new Vector3(x, y, transform.position.z);
+
+        transform.position = new Vector3(radius * Mathf.Cos(currentAngle), radius * Mathf.Sin(currentAngle), transform.position.z);
 
         transform.LookAt(Vector3.zero);
     }

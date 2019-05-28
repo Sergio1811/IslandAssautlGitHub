26459fd3c90a8095a9e62c354cdf6bc46dc95042
sergio1811 <pekeno1811@gmail.com>
2019-05-28 18:35:56 +0200

    NubesMov

--- Assets/Scripts/RotateTurnTable.cs
+++ Assets/Scripts/RotateTurnTable.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class RotateTurnTable : MonoBehaviour
 {
+    float speed;
    
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = Random.Range(0.5f, 1.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.gameObject.transform.Rotate(this.gameObject.transform.up, 10 *Time.deltaTime); 
+        this.gameObject.transform.Rotate(this.gameObject.transform.up, speed *Time.deltaTime); 
     }
 }

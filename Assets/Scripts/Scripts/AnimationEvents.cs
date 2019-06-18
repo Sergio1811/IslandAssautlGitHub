37a863fca3ffca9a49b0b5e87132b5a5747a925f
Yaiza <yaizacastillo@users.noreply.github.com>
2019-06-18 17:15:46 +0200

    Pasos

---
+++ Assets/Scripts/Scripts/AnimationEvents.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEvents : MonoBehaviour
+{
+    public void Step()
+    {
+        SoundManager.PlayOneShot(SoundManager.StepSound, GameManager.Instance.cameraAnchor.transform.position);
+    }
+}
